a0b6e8b089b17c86cec919e7235273bab442e0b9
acheraga <abdelhamid.cheraga@parisnanterre.fr>
2022-06-30 16:16:41 +0200

    maj-docker run

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#docker stop apogee > /dev/null 2>&1
-#docker rm apogee > /dev/null 2>&1
+docker stop esup-siscol > /dev/null 2>&1
+docker rm esup-siscol > /dev/null 2>&1
 image_tag=0.1.14
 docker run -d -p 8080:8080  --name="esup-siscol" local/esup-siscol:v0.1.14 --dns=193.50.151.50
-docker logs -f apogee 
+#docker logs -f esup-sisco
